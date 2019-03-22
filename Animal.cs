632510d229025c8e0e3ff52756f574f770cfe3ee
Jordan Bryan <jbryan382@gmail.com>
2019-03-22 19:38:16 -0400

    ORM Up and running, GetGetPush

---
+++ Animal.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace OnlineSafari
+{
+  public class Animal
+  {
+    public int Id { get; set; }
+    public string Species { get; set; }
+    public int CountOfTimesSeen { get; set; }
+    public string LocationOfLastSeen { get; set; }
+  }
+}
---
+++ Controllers/AnimalsController.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using onlinesafari;
+
+namespace OnlineSafari.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class AnimalsController : ControllerBase
+  {
+
+    [HttpGet]
+    public ActionResult<IList<Animal>> GetAllSpecies()
+    {
+      var db = new DatabaseContext();
+      var animals = db.Animals.OrderBy(o => o.Species).ToList();
+      return animals;
+
+    }
+    [HttpGet("{id}")]
+    public ActionResult<Animal> GetChosenSpecies(int id)
+    {
+      var db = new DatabaseContext();
+      var animal = db.Animals.FirstOrDefault(f => f.Id == id);
+      return animal;
+    }
+
+    [HttpPost]
+    public ActionResult<Animal> Post([FromBody] Animal newAnimal)
+    {
+      var db = new DatabaseContext();
+      db.Animals.Add(newAnimal);
+      db.SaveChanges();
+      return newAnimal;
+    }
+
+  }
+}
