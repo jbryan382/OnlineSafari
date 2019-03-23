ea1f626ed7a0f8212dccaa0b20e9be59de7807f3
Jordan Bryan <jbryan382@gmail.com>
2019-03-22 21:41:14 -0400

    Added Update and Delete

--- Controllers/AnimalsController.cs
+++ Controllers/AnimalsController.cs
@@ -43,5 +43,27 @@ namespace OnlineSafari.Controllers
       return newAnimal;
     }
 
+    [HttpPut("{id}")]
+    public ActionResult<Animal> UpdateMovie(int id, [FromBody] Animal addToAnimal)
+    {
+      var db = new DatabaseContext();
+      var animal = db.Animals.FirstOrDefault(f => f.Id == id);
+      animal.Species = addToAnimal.Species;
+      animal.CountOfTimesSeen = addToAnimal.CountOfTimesSeen;
+      animal.LocationOfLastSeen = addToAnimal.LocationOfLastSeen;
+      db.SaveChanges();
+      return animal;
+    }
+
+    [HttpDelete("{id}")]
+    public ActionResult DeleteMovie(int id)
+    {
+      var db = new DatabaseContext();
+      var animal = db.Animals.FirstOrDefault(f => f.Id == id);
+      db.Animals.Remove(animal);
+      db.SaveChanges();
+      return Ok();
+    }
+
   }
 }
